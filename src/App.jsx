0ba11bbf26ed915774a62dd713fed9655cfbf86a
diegodiego1989 <hosana.dev@gmail.com>
2022-12-06 13:58:54 +0000

    added temporary message

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,24 @@ function App() {
 
   return (
     <div className={styles.app}>
+      {/* Temporário */}
+      <div style={
+        {
+          backgroundColor: "yellow",
+          width: "100%",
+          position: "fixed",
+          top: "-0.05%",
+          left: "0",
+          padding: "1.1rem",
+          fontWeight: "700",
+          textAlign: "center",
+          letterSpacing: "0.75px",
+          textTransform: "uppercase",
+          fontSize: "1.6rem",
+          
+
+        }}> Under development </div>
+
       <MainComponent
       />
     </div>
