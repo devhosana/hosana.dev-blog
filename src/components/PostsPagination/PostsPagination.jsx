40e032b307e5406b11693e57514e74f9ff66bc79
diegodiego1989 <hosana.dev@gmail.com>
2022-12-08 14:57:30 +0000

    fixed bugs on posts pagination

--- src/components/PostsPagination/PostsPagination.jsx
+++ src/components/PostsPagination/PostsPagination.jsx
@@ -8,9 +8,23 @@ import arrowRight from "../../assets/icons/arrow_right.png";
 
 function PostsPagination({ pages, currentPostsPosition, postsPaginationHandler }) {
 
+  let multiplier = 0;
+
   const onClickHandler = function(event) {
     const sideOrNumber = event.target?.dataset.side || event;
     postsPaginationHandler(sideOrNumber);
+  };
+
+  const loopUntilRemainderEqualsZero = function(currentPaginationPosition) {
+
+    let currPagPos = currentPaginationPosition;
+
+    while (currPagPos % 5 !== 0) {
+      currPagPos < 0 ? currPagPos-- : currPagPos ++;
+    };
+
+    return Math.abs(currPagPos);
+
   };
 
   const [pagesNavigationDivider, setPagesNavigationDivider] = useState(4);
@@ -18,24 +32,32 @@ function PostsPagination({ pages, currentPostsPosition, postsPaginationHandler }
   
   useEffect(() => {
 
-    if ((currentPostsPosition + 1) % 5 === 0) {
-      setPagesNavigationDivider(previousDividerValue => {
-        if (currentPostsPosition >= previousDividerValue) return previousDividerValue + 5;
-        if (currentPostsPosition < previousDividerValue) return previousDividerValue - 5;
-      });
-    } else {
-      // Provavelmente solução está encaixar logica no else aqui
-      console.log(currentPostsPosition % 5);
-      console.log(currentPostsPosition);
-    }; 
+    const currentPaginationPosition = pagesNavigationDivider - currentPostsPosition;
     
-    // Parei aqui, falta achar lógica pra ele voltar quando chegamos em 5 e voltamos
-    // Ao fazermos F5 e clicarmos diretamente no último post (depois das reticencias também ocorre bug)
-    // tentar encaixar algum condicional com && no segundo if
-    // console.log(currentPostsPosition + 1);
+    if (currentPaginationPosition < 0 && currentPaginationPosition > -6) {
+      setPagesNavigationDivider(previousState => previousState + 5);
+      console.log(pagesNavigationDivider);
+    };
+    
+    if (currentPaginationPosition >= 5) {
+      setPagesNavigationDivider(previousState => previousState - 5);
+    };
 
+    if (-5 > currentPaginationPosition) {
+      const lastPaginationPosition = loopUntilRemainderEqualsZero(currentPaginationPosition) + 4;
+      setPagesNavigationDivider(lastPaginationPosition);
+    };
+
+    /*
+    Later 1
+    if (currentPaginationPosition > 5) {
+      const lastPaginationPosition = loopUntilRemainderEqualsZero(currentPaginationPosition) - 4;
+      setPagesNavigationDivider(lastPaginationPosition);
+    };
+    */
+    
   }, [currentPostsPosition]);
-
+  
 
   return (
     <div className={styles.postsNav}>
@@ -49,7 +71,10 @@ function PostsPagination({ pages, currentPostsPosition, postsPaginationHandler }
         />}
       </div>
 
-      {/* <div
+        
+      {/*
+      Later 2
+      <div
         className={styles.pageNumber}
         onClick={() => onClickHandler(0)}
       >{pages.length + 1 - pages.length}</div>
@@ -58,8 +83,8 @@ function PostsPagination({ pages, currentPostsPosition, postsPaginationHandler }
 
       {pages.map((_, index) => {
         return (
-        // Lógica p/ divisão de paginação ocorrer de 5 em 5 posições
-          index <= pagesNavigationDivider && index >= pagesNavigationDivider - 5 &&
+          // Lógica p/ divisão de paginação ocorrer de 5 em 5 posições
+          index <= pagesNavigationDivider && index > (pagesNavigationDivider - 5) &&
             <div
               className={`${styles.pageNumber} ${index === currentPostsPosition && styles.pageNumberCurrent}`}
               onClick={() => onClickHandler(index)}
